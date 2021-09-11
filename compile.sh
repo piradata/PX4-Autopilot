1824e015d22b123f8477c3ef191e795023b83ea0
piradata <guilhermefercar@gmail.com>
2021-09-11 14:32:31 -0300

    scripts

--- compile.sh
+++ compile.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sudo make px4_sitl_default gazebo_iris PX4_SITL_WORLD:=$(pwd)/Tools/sitl_gazebo/worlds/uneven.world
+sudo make px4_sitl_default gazebo_iris PX4_SITL_WORLD:=$(pwd)/Tools/sitl_gazebo/worlds/empty.world
+
+# sudo make px4_sitl_default gazebo_iris PX4_SITL_WORLD:=$(pwd)/Tools/sitl_gazebo/worlds/uneven.world
+
+# sudo make px4_sitl_default gazebo_iris PX4_SITL_WORLD:=$(pwd)/Tools/sitl_gazebo/worlds/iris.world
+
+# sudo make px4_sitl_default gazebo_iris PX4_SITL_WORLD:=$(pwd)/Tools/sitl_gazebo/worlds/baylands.world
---
+++ compile_grama.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# sudo make no_sim=1 px4_sitl_default gazebo &
+
+# source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default
+
+# roslaunch gazebo_ros empty_world.launch world_name:=$(pwd)/Tools/sitl_gazebo/worlds/navio.world
+
+sudo make px4_sitl_default gazebo_iris PX4_SITL_WORLD:=$(pwd)/Tools/sitl_gazebo/worlds/gramado.world
